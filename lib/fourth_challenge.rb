d991b00cf7f865c420da0f495b89c92dd8c48562
Christoph Gruener <christophgruener@me.com>
2019-01-30 18:31:42 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,7 +22,7 @@ def fourth_challenge
             :age => "30",
             :attitude => "confused" },
             {
-            :mame => "Nurse",
+            :name => "Nurse",
             :age => "44",
             :attitude => "worried"}
       ]
