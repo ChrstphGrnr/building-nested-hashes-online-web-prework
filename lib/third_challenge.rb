f38abeeee0fb6544f34f1bcbc8a991ba08399eff
Christoph Gruener <christophgruener@me.com>
2019-01-30 18:28:02 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,26 +6,25 @@ def third_challenge
       :hero => { :name => "Romeo", :age => "15", :status => "alive" },
       :hero_friends => [
         :benvolio => {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"},
+          :name => "Benvolio", 
+          :age => "17", 
+          :attitude => "worried"}, 
         :mercutio =>{
-          :name => "Mercutio",
-          :age => "18",
+          :name => "Mercutio", 
+          :age => "18", 
           :attitude => "hot-headed"}]
     },
    :capulet => {
       :patriarch => { :name => "Lord Capulet", :age => "50" },
       :matriarch => { :name => "Lady Capulet", :age => "51" },
       :heroine => { :name => "Juliet", :age => "15", :status => "alive" },
-      :heroine_friends => [
-          :steven => {
-            :name => "Steven",
+      :heroine_friends => [{
+            :name => "Steven", 
             :age => "30",
-            :attitude => "confused" },
-          :nurse => {
-            :mame => "Nurse",
-            :age => "44",
+            :attitude => "confused" }, 
+            {
+            :mame => "Nurse", 
+            :age => "44", 
             :attitude => "worried"}]
     }
   }
